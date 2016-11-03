15667dc1a524c2dda6a83533d44160b517d8d22c
Krystyna Taheri <krystyna.taheri@yale.edu>
2016-11-03 14:11:21 +0000

    all of kyles fancy stuff - take it!

--- config/routes.js
+++ config/routes.js
@@ -3,19 +3,21 @@ var env = process.env.NODE_ENV || 'development',
 
 module.exports = function (app) {
   //=============================
-  // Static
+  // Routes and controllers for tasks
   //=============================
+  var taskControllers = require('../app/controllers/task');
   app.get('/', function(req, res) {
     res.send('woot kyle is here');
   });
+  app.get('/tasks', taskControllers.showTasks);
 
   //=============================
   // Bookmark
   //=============================
-  var person = require('../app/controllers/person');
-  app.get('/person/', person.index);
-  app.get('/person/:id', person.show);
-  app.post('/person/', person.create);
-  app.put('/person/:id', person.update);
-  app.del('/person/:id', person.delete);
+  // var person = require('../app/controllers/person');
+  // app.get('/person/', person.index);
+  // app.get('/person/:id', person.show);
+  // app.post('/person/', person.create);
+  // app.put('/person/:id', person.update);
+  // app.del('/person/:id', person.delete);
 };
